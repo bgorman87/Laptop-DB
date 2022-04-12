b4c4f4962f5a93975b139a35b0e1972d05fb7485
bgorman87 <bgorman@live.com>
2022-04-12 13:20:14 +0000

    removed static setting

--- parts/parts/settings_prod.py
+++ parts/parts/settings_prod.py
@@ -201,7 +201,6 @@ if USE_S3:
     AWS_S3_REGION_NAME = get_env_variable('AWS_S3_REGION_NAME')
     AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
     STATIC_LOCATION = 'static'
     # STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
