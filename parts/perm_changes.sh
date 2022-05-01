2e20b9ec16214494057154c063a7578b36a0d8be
bgorman87 <bgorman@live.com>
2022-05-01 15:56:50 +0000

    AfterInstall perm change script for appspec

---
+++ parts/perm_changes.sh
@@ -0,0 +1,3 @@
+sudo chown -R www-data:www-data /home/ubuntu/django/parts/parts/
+sudo chown www-data:www-data /home/ubuntu/django/parts/parts/db.sqlite3
+sudo chmod 775 /home/ubuntu/django/parts/parts/db.sqlite3
